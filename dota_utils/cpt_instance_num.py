bb2792557a294231df63c255613f613cb2fd5c59
GaoShanwen <40156715+GaoShanwen@users.noreply.github.com>
2019-09-04 01:38:14 +0800

    Update cpt_instance_num.py

--- dota_utils/cpt_instance_num.py
+++ dota_utils/cpt_instance_num.py
@@ -1,18 +1,21 @@
-import dota_utils as util
+import sys
 import os
-import cv2
-import json
-import shutil
-import numpy as np
-import math
-from DOTA_devkit import polyiou
+sys.path.append('./')
+from DOTA_devkit.dota_utils import wordname_15
+from DOTA_devkit import dota_utils as util
+# import cv2
+# import json
+# import shutil
+# import numpy as np
+# import math
+# from DOTA_devkit import polyiou
 import collections
 
-wordname_18 = ['__background__',
-            'airport', 'baseball-diamond', 'basketball-court', 'bridge', 'container-crane',
-            'ground-track-field', 'harbor', 'helicopter', 'helipad', 'large-vehicle',
-            'plane', 'roundabout', 'ship', 'small-vehicle', 'soccer-ball-field',
-            'storage-tank', 'swimming-pool', 'tennis-court']
+# wordname_18 = ['__background__',
+#             'airport', 'baseball-diamond', 'basketball-court', 'bridge', 'container-crane',
+#             'ground-track-field', 'harbor', 'helicopter', 'helipad', 'large-vehicle',
+#             'plane', 'roundabout', 'ship', 'small-vehicle', 'soccer-ball-field',
+#             'storage-tank', 'swimming-pool', 'tennis-court']
 
 
 def dots4ToRec8(bbox):
@@ -28,15 +31,15 @@ def multi_mkdir(path):
     os.mkdir(path)
 
 
-def DOTA2COCO(srcpath, dest_dir):
+def DOTA2COCO(srcpath, dest_dir, category_list):
     multi_mkdir(dest_dir)
-    labelparent = os.path.join(srcpath, 'labelTxt')
+    labelparent = os.path.join(srcpath, 'labelTxt-v1.0')
 
     inst_count = 0
     image_id = 0
     categories_dict = {}
     data_dict = {}
-    for category in wordname_18:
+    for category in category_list:
         if category == '__background__':
             continue
         categories_dict[category] = collections.OrderedDict({'T': 0, 'S': 0, 'M': 0, 'L': 0, 'total': 0})
@@ -55,6 +58,9 @@ def DOTA2COCO(srcpath, dest_dir):
             inst_count = inst_count + 1
             single_obj = {}
             single_obj['filename'] = basename
+            xmin, ymin, xmax, ymax = min(obj['poly'][0::2]), min(obj['poly'][1::2]), \
+                                     max(obj['poly'][0::2]), max(obj['poly'][1::2])
+            obj['area'] = (xmax - xmin) * (ymax - ymin)
             if obj['area']  <16*16:
                 categories_dict[obj['name']]['T'] += 1
             elif obj['area']<32*32:
@@ -63,8 +69,6 @@ def DOTA2COCO(srcpath, dest_dir):
                 categories_dict[obj['name']]['M'] += 1
             else:
                 categories_dict[obj['name']]['L'] += 1
-            xmin, ymin, xmax, ymax = min(obj['poly'][0::2]), min(obj['poly'][1::2]), \
-                                     max(obj['poly'][0::2]), max(obj['poly'][1::2])
             single_obj['bbox'] = dots4ToRec8([xmin, ymin, xmax, ymax])
             data_dict[obj['name']].append(single_obj)
             # data_dict['annotations']
@@ -74,10 +78,13 @@ def DOTA2COCO(srcpath, dest_dir):
         #     break
     # print(data_dict)
     if 'val' in dest_dir:
-        print('val/name:'.ljust(24), 'T:'.ljust(24), 'S:'.ljust(24), 'M:'.ljust(24), 'L:'.ljust(24))
+        print('| val/name:'.ljust(24), '| T:'.ljust(24), '| S:'.ljust(24), '| M:'.ljust(24),
+              '| L:'.ljust(24), '| Total |'.ljust(26))
     else:
-        print('train/name:'.ljust(24), 'T:'.ljust(24), 'S:'.ljust(24), 'M:'.ljust(24), 'L:'.ljust(24))
-    for category in wordname_18:
+        print('| train/name:'.ljust(24), '| T:'.ljust(24), '| S:'.ljust(24), '| M:'.ljust(24),
+              '| L:'.ljust(24), '| Total |'.ljust(26))
+    print('| - | - | - | - | - | - |')
+    for category in category_list:
         if category == '__background__':
             continue
         categories_dict[category]['total'] = categories_dict[category]['T'] + categories_dict[category]['S'] \
@@ -91,11 +98,12 @@ def DOTA2COCO(srcpath, dest_dir):
                 line = line.replace('(', '').replace(',', '').replace(')', '')
                 save_f.writelines(line + '\n')
         save_f.close()
-        print('{}'.format(category).ljust(24), '{}'.format(100* categories_dict[category]['T']/categories_dict[category]['total']).ljust(24),
-              '{}'.format(100* categories_dict[category]['S'] / categories_dict[category]['total']).ljust(24),
-              '{}'.format(100* categories_dict[category]['M'] / categories_dict[category]['total']).ljust(24),
-              '{}'.format(100* categories_dict[category]['L'] / categories_dict[category]['total']).ljust(24),
-              '{}'.format(categories_dict[category]['total']))
+        print('| {}'.format(category).ljust(24),
+              '| {}'.format(100* categories_dict[category]['T']/categories_dict[category]['total']).ljust(24),
+              '| {}'.format(100* categories_dict[category]['S'] / categories_dict[category]['total']).ljust(24),
+              '| {}'.format(100* categories_dict[category]['M'] / categories_dict[category]['total']).ljust(24),
+              '| {}'.format(100* categories_dict[category]['L'] / categories_dict[category]['total']).ljust(24),
+              '| {} |'.format(categories_dict[category]['total']))
             # print(line)
             # break
         categories_dict['all']['T'] += categories_dict[category]['T']
@@ -104,21 +112,18 @@ def DOTA2COCO(srcpath, dest_dir):
         categories_dict['all']['L'] += categories_dict[category]['L']
         categories_dict['all']['total'] += categories_dict[category]['total']
         # break
-    print('{}'.format('all').ljust(24),
-          '{}'.format(100 * categories_dict['all']['T'] / categories_dict['all']['total']).ljust(24),
-          '{}'.format(100 * categories_dict['all']['S'] / categories_dict['all']['total']).ljust(24),
-          '{}'.format(100 * categories_dict['all']['M'] / categories_dict['all']['total']).ljust(24),
-          '{}'.format(100 * categories_dict['all']['L'] / categories_dict['all']['total']).ljust(24),
-          '{}'.format(categories_dict['all']['total']))
+    print('| {}'.format('all').ljust(24),
+          '| {}'.format(100 * categories_dict['all']['T'] / categories_dict['all']['total']).ljust(24),
+          '| {}'.format(100 * categories_dict['all']['S'] / categories_dict['all']['total']).ljust(24),
+          '| {}'.format(100 * categories_dict['all']['M'] / categories_dict['all']['total']).ljust(24),
+          '| {}'.format(100 * categories_dict['all']['L'] / categories_dict['all']['total']).ljust(24),
+          '| {} |'.format(categories_dict['all']['total']))
     print('img:{}, ins:{}'.format(image_id, inst_count))
     # print(categories_dict)
     # json.dump(data_dict, f_out)
 
 
 if __name__ == '__main__':
-    DOTA2COCO(r'./data/DOTA/data_ori/train', r'./data/cpt_train_label')
-    DOTA2COCO(r'./data/DOTA/data_ori/val', r'./data/cpt_val_label')
-    # DOTA2COCO(r'./data/DOTA/data_last/train_800', r'./data/DOTA/annotation/sub800_train2019.json')
-    # DOTA2COCO(r'./data/DOTA/data_last/val_800', r'./data/DOTA/annotation/sub800_val2019.json')
-
+    DOTA2COCO(r'./data/DOTA-v1/data_ori/train', r'./result/cpt_Hbox_train_label', wordname_15)
+    DOTA2COCO(r'./data/DOTA-v1/data_ori/val', r'./result/cpt_Hbox_val_label', wordname_15)
 
